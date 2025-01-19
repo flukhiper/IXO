4a6ab1e585998615b33f88d7c1473aded2857c98
Chaowakrit <chaowakrit@thinknet.co.th>
2025-01-19 11:46:09 +0700

    fix .gitignore

--- client-manual/client/.vite/deps/react-dom.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-JFZDBQVP.js";
-import "./chunk-3C5AF2YN.js";
-export default require_react_dom();
--- client-manual/client/.vite/deps/react.js
+++
@@ -1,4 +0,0 @@
-import {
-  require_react
-} from "./chunk-3C5AF2YN.js";
-export default require_react();
