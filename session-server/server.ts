cc227db2333b0d3467e78d043be2ba04fa126c24
Chaowakrit <chaowakrit@thinknet.co.th>
2025-03-18 00:03:16 +0700

    - latest edit code before start the new one

--- session-server/server.ts
+++ session-server/server.ts
@@ -1,7 +1,10 @@
 // @ts-types="@types/express"
 import express from "express";
+import cors from "cors";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+
+import SessionManager from "@/services/SessionManager.ts";
 
 const app = express();
 const server = createServer(app);
@@ -11,12 +14,39 @@ const io = new Server(server, {
   },
 });
 
+const sessionManager = new SessionManager();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.json("server running at http://localhost:3001");
 });
 
+app.post("/session", (req, res) => {
+  const { name } = req.body;
+  const session = sessionManager.createSession(name);
+  res.json(session);
+});
+
+app.get("/session", (req, res) => {
+  const sessions = sessionManager.getSessionList();
+  res.json(sessions);
+});
+
 io.on("connection", (socket) => {
   console.log("New client connected:", socket.id);
+
+  socket.on("join_session", (data) => {
+    const { playerID, sessionID } = data;
+    const player = {
+      id: playerID,
+      name: "Player " + playerID,
+      isTyping: false,
+    };
+    sessionManager.joinSession(sessionID, player, socket);
+  });
 
   // Handle incoming messages
   socket.on("message", (msg) => {
