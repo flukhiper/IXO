b378d0ba46b7b2a1e1a486b91a077478068d5f96
Chaowakrit <chaowakrit@thinknet.co.th>
2025-05-12 21:30:38 +0700

    - add tab bar

--- ixo-app/src/app/demo/page.tsx
+++ ixo-app/src/app/demo/page.tsx
@@ -4,17 +4,23 @@ import { SelectionProvider } from '@/contexts/SelectionContext';
 
 import StageComponent from './components/Stage';
 import Tools from './components/Tools';
+import Menu from './components/menu/Menu';
 
 const DemoPage = () => {
 
   return <>
     <GameProvider>
       <SelectionProvider>
-        <div className='min-h-screen p-5 flex flex-col'>
-          <h1>Demo</h1>
-          <Tools/>
-          <ClickCounter/>
-          <StageComponent />
+        <div className='flex'>
+          <div className='min-h-screen p-5 flex flex-col flex-1'>
+            <h1>Demo</h1>
+            <Tools/>
+            <ClickCounter/>
+            <StageComponent />
+          </div>
+          <div>
+            <Menu />
+          </div>
         </div>
       </SelectionProvider>
     </GameProvider>
